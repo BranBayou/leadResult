dd5e6ffca030149610e6d2f687106099c5b72dec
Berhanu Baye <berhan.baye@gmail.com>
2024-12-13 19:35:11 +0300

    Add event lister for buttons

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ const tooltip = document.getElementById('tooltip');
     function hideTooltip() {
       tooltip.textContent = "";
     }
+
+// Event listeneer for dount charts scroll //
+const chartsDiv = document.querySelector('.charts-div');
+const nextButton = document.querySelector('.next-button');
+const prevButton = document.querySelector('.prev-button');
+
+const scrollAmount = 300;
+
+// Scroll to the right
+nextButton.addEventListener('click', () => {
+  chartsDiv.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+});
+
+// Scroll to the left
+prevButton.addEventListener('click', () => {
+  chartsDiv.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+});
